6672c12f50d531324b573eb78f82894c6b53a2ae
Gaurav Kulkarni <kgaurav@umich.edu>
2012-03-25 22:08:07 -0300

    Added page header to photo.php

--- photo.php
+++ photo.php
@@ -22,6 +22,19 @@ if(!$user)
 	<link rel="stylesheet" type="text/css" href="assets/bootstrap.min.css" />
 </head>
 <body>
-	<div class="container" style="margin-top: 20px"></div>
+	<div class="container" style="margin-top: 20px">
+	<?php
+		$average_like = 0;
+	
+		echo
+			"<div class='page-header'>
+				<h1>
+					On average, ".round($average_like, 2)." people like photos you are tagged in
+					<small>Congratulations</small>
+				</h1>
+			</div>";
+	
+	?>
+	</div>
 </body>
 </html>
